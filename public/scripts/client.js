e8542240870fe0ea3f09c3448a746b661c201ce1
Nolan Raadt <thegodfatherpart2@Nolans-MBP.usinternet.com>
2017-02-02 19:45:24 -0600

    gif random working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,57 @@ var app = angular.module('giphyApp', []);
 app.controller('GiphyController', function($http){
   console.log('GiphyController loaded');
 
-  var API = 'http://api.giphy.com/v1/gifs'
-
   var ctrl = this;
+  var API = 'http://api.giphy.com/v1';
 
-  ctrl.themGifs = [];
+  ctrl.imageUrl = ' ';
+  ctrl.searchQuery = ' ';
+  ctrl.searchGifs = [];
 
-  $http.get(API + '/random?api_key=dc6zaTOxFJmzC').then(function(response){
-    console.log('Got a response from the API', response);
-    // ctrl.pokemonList = response.data.results;
-  }).catch(function(err){
-    console.log('Error getting info from API', err);
-  });
-
-  ctrl.grabGif = function(gif){
-    console.log('Chose', gif);
-    $http.get(API + '/random?api_key=dc6zaTOxFJmzC').then(function(response){
-      console.log('GIF info', response.data);
+  ctrl.grabGif = function() {
+    $http({
+      method: 'GET',
+      url: API + '/gifs/random',
+      params: {
+        api_key: 'dc6zaTOxFJmzC',
+        q: null
+      }
+    }).then(function(response) {
+      console.log((response.data.data.image_url));
       ctrl.imageUrl = response.data.data.image_url;
-    }).catch(function(err){
-      console.log('Error getting info from API', err);
+    }).catch(function(err) {
+      console.log('Error getting data', err);
     });
   }
 
-  ctrl.searchGif = function(gif){
-    // console.log('Chose', gif);
-    var searched = gif;
-    var replaced = searched.split(' ').join('+');
-    console.log('They searched for: ',replaced);
-    $http.get(API + '/search?q=' +  + '&api_key=dc6zaTOxFJmzC').then(function(response){
-      console.log('GIF info', response.data);
-      ctrl.imageUrl = response.data.data.image_url;
-    }).catch(function(err){
-      console.log('Error getting info from API', err);
-    });
-  }
 
+  //
+  // $http.get(API + '/random?api_key=dc6zaTOxFJmzC').then(function(response){
+  //   console.log('Got a response from the API', response);
+  // }).catch(function(err){
+  //   console.log('Error getting info from API', err);
+  // });
+
+
+  ctrl.searchGif = function() {
+    console.log('In search fxn now');
+    $http({
+      method: 'GET',
+      url: API + '/gifs/search',
+      params: {
+        api_key: 'dc6zaTOxFJmzC',
+        q: ctrl.searchQuery
+      }
+    }).then(function(response) {
+      console.log('Responded with', response);
+    }, function errorCallback(response) {
+      console.log('Error', response);
+    });
+    // }).then(function(response) {
+    //   console.log(('The search returned ', response.data.data));
+    //   ctrl.searchGifs = response.data.data;
+    // }).catch(function(err) {
+    //   console.log('Error getting data', err);
+    // });
+  }
 });
